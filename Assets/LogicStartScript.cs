2b83e2ece7438ebc557318a453bac9f6ebbe75e5
trangn03 <trangn0102@csu.fullerton.edu>
2024-02-20 13:40:44 -0800

    Update the button of Begin Play
    When the user click the button "Play Game", it will automatically head to the Play Game scene

---
+++ Assets/LogicStartScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicStartScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void startGame()
+    {
+        SceneManager.LoadScene("Game Play");
+    }
+}
